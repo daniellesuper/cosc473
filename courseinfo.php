ab42f2faccc30e9b70bde7ad86242a28907a8b67
daniellesuper <danielle.super7@gmail.com>
2020-02-12 11:12:46 -0500

    Update courseinfo.php

--- courseinfo.php
+++ courseinfo.php
@@ -46,25 +46,6 @@ $topicname1 =$row[topicname1];
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 <script src="https://cdn.ckeditor.com/4.11.2/standard/ckeditor.js"></script>
 <script src="https://cdn.ckeditor.com/ckeditor5/11.2.0/classic/ckeditor.js"></script>
-<header>
-	<nav class="navbar navbar-inverse">
-    <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="index.html">InfoSyllabus&copy;</a>
-    </div>
-    <ul class="nav navbar-nav">
-      <li class="active"><a href="#">Home</a></li>
-      <li><a href="about.php">About</a></li>
-      <li><a href="contact.php">Contact</a></li>
-    </ul>
-    <ul class="nav navbar-nav navbar-right">
-      <li><a href="logout.php"><span class="glyphicon glyphicon-log-in"></span> Logout</a></li>
-    </ul>
-  </div>
-</nav>
-	
-	<hr />
-	</header>
 
 <head>
 	<link href="mainpage.css" type="text/css" rel="stylesheet" />
